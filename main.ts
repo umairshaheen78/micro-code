df6db0c6cdab604e6ef4893341add1fc425c00ca
Umair Shaheen <umairshaheen7@gmail.com>
2022-09-28 14:22:26 -0700

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -15,13 +15,99 @@ input.onButtonPressed(Button.A, function () {
         `)
 })
 input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        `)
     basic.showString("A = ")
     basic.showNumber(2025)
 })
 input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        `)
     basic.showString("Solve")
     basic.pause(1000)
     basic.showString("45^2")
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . # .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . # . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . .
+        `)
 })
 basic.pause(1000)
 basic.showString("Start?")
